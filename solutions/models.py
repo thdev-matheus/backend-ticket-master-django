ba1d68ea4271939ad07df9d536003cf1f332b88b
Andre Perregil <andre_perregil@outlook.com>
2022-10-31 15:02:31 +0000

    feat: solutions.models implemented, awaiting changes to pull from dev branch

--- solutions/models.py
+++ solutions/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+import uuid
 
-# Create your models here.
+class Solution(models.Model):
+    id = models.UUIDField(default=uuid.uuid4, primary_key=True, editable=False)
+    description = models.TextField()
+    solved_at = models.DateTimeField(auto_now_add=True)
+    time_taken = models.CharField(max_length=30)
+    ticket_id = models.ForeignKey(
+        "tickets.Ticket",
+        on_delete=models.CASCADE,
+        related_name="solution"
+    )
+    user_id = models.ForeignKey(
+        "users.User",
+        on_delete=models.CASCADE,
+        related_name="solved_by"
+    )
+
