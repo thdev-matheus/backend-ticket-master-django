6322367dc1206799fbe92d749f1a1c756c83aaf1
Bruno <brunopassos_bp@hotmail.com>
2022-10-31 16:19:23 -0300

    feat: permissions implemented to list and create users

--- users/views.py
+++ users/views.py
@@ -1,10 +1,18 @@
 from rest_framework import generics
 
 from users.models import User
+from users.permissions import IsAdm
 
 from users.serializers import UserSerializer
 
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.authentication import TokenAuthentication
+
 
 class UserView(generics.ListCreateAPIView):
+
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAuthenticated, IsAdm]
+
     queryset = User.objects.all()
     serializer_class = UserSerializer
