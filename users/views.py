fa6ff2d89f9112a64c88d51d06428a25a613c62d
Bruno <brunopassos_bp@hotmail.com>
2022-10-31 17:22:32 -0300

    feat: user update implemented

--- users/views.py
+++ users/views.py
@@ -11,15 +11,17 @@ from rest_framework.authentication import TokenAuthentication
 
 class UserView(generics.ListCreateAPIView):
 
-    # authentication_classes = [TokenAuthentication]
-    # permission_classes = [IsAuthenticated, IsAdm]
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAuthenticated, IsAdm]
 
     queryset = User.objects.all()
     serializer_class = UserSerializer
 
 class UserUpdateView(generics.UpdateAPIView):
-    # authentication_classes = [TokenAuthentication]
-    # permission_classes = [IsAuthenticated, IsAdm]
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAuthenticated, IsAdm]
 
     queryset = User.objects.all()
     serializer_class = UserSerializer
+
+    lookup_url_kwarg = "user_id"
