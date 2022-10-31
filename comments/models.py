9214f50d442a18bc3470b7cadc4336882629b6e8
Heitor <heitor.cardoso39@gmail.com>
2022-10-31 13:48:41 -0300

    fix: models relations fixed

--- comments/models.py
+++ comments/models.py
@@ -7,6 +7,6 @@ class Comment(models.Model):
     id = models.UUIDField(default=uuid.uuid4, primary_key=True, editable=False)
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
-    ticket_id = models.ForeignKey(
+    ticket = models.ForeignKey(
         "tickets.Ticket", on_delete=models.CASCADE, related_name="comments"
     )
