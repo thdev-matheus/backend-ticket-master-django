c7cb7b70e1b4b15613240e5e03d57b060a54baab
Andre Perregil <andre_perregil@outlook.com>
2022-11-01 17:08:52 +0000

    feat: inital ticket views implemented

--- tickets/urls.py
+++ tickets/urls.py
@@ -4,5 +4,8 @@ from tickets import views
 
 urlpatterns = [
     path("tickets/", views.ListCreateTicketsView.as_view()),
-    
+    path("tickets/<ticket_id>/", views.ListPatchSolveTicketView.as_view()),
+    path("summary/tickets/", views.ListTicketTotalsView.as_view()),
+    path("tickets/department/<department_id>/", views.ListTicketsFromDepartmentView.as_view()),
+    path("tickets/department/<department_id>/newest/", views.ListMostUrgentTicketView.as_view())
 ]
