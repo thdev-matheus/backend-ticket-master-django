abc13121113fb3a085d7457600c332c8690c1d18
Matheus Vieira <109465340+thdev-matheus@users.noreply.github.com>
2022-11-01 09:26:40 -0300

    refactor: update UserSerializer

--- users/serializers.py
+++ users/serializers.py
@@ -1,19 +1,19 @@
-from rest_framework import serializers
-
 from users.models import User
 
 
 class UserSerializer(serializers.ModelSerializer):
-    password = serializers.CharField(write_only=True)
 
     class Meta:
             model = User
-            fields = ["id","username", "department_id", "is_superuser", "password"]
+            fields = ["id","username", "department", "is_superuser", "password"]
             read_only_fields = ["id", "is_superuser"]
+            extra_kwargs = {
+                "password": {
+                    "write_only": True,
+                },
+            }
 
     def create(self, validated_data):
-        # user = User(**validated_data)
-        # user.set_password(validated_data['password'])
-        # user.save()
         model = self.Meta.model
-        model.create_user(**validated_data)
+        instance = model.create_user(**validated_data)
+        return instance
