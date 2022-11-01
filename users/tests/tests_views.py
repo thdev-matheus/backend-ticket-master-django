5b273216ee208bc5e6966162ac0b9a532d8a0522
Theus <matheusth.dev@gmail.com>
2022-11-01 15:09:20 -0300

    feat: test UserReactivateView setup test

--- users/tests/tests_views.py
+++ users/tests/tests_views.py
@@ -267,4 +267,37 @@ class UserDeleteViewTest(APITestCase):
 
 
 class UserReactivateViewTest(APITestCase):
-    ...
+    @classmethod
+    def setUpTestData(cls) -> None:
+        cls.user_admin = User.objects.create_superuser(
+            username="AdminUser", password="1234"
+        )
+        cls.user_non_admin = User.objects.create_user(
+            username="NonAdminUser", password="1234"
+        )
+
+        cls.user_admin_data_login = {
+            "username": cls.user_admin.username,
+            "password": "1234",
+        }
+
+        cls.user_non_admin_data_login = {
+            "username": cls.user_non_admin.username,
+            "password": "1234",
+        }
+
+        cls.test_update_data = {
+            "username": "user atualizado",
+        }
+
+    def setUp(self) -> None:
+        login_admin = self.client.post("/api/login/", self.user_admin_data_login)
+        login_non_admin = self.client.post(
+            "/api/login/", self.user_non_admin_data_login
+        )
+
+        self.token_admin = f"Token {login_admin.data['token']}"
+        self.token_non_admin = f"Token {login_non_admin.data['token']}"
+
+        self.client.credentials(HTTP_AUTHORIZATION=self.token_admin)
+        self.client.delete(f"/api/users/{self.user_non_admin.id}/")
