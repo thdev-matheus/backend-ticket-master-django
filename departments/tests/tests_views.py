b156ae63137ba829e42169e2fc36415a60036aeb
Theus <matheusth.dev@gmail.com>
2022-11-02 23:22:04 -0300

    feat: tests departments PatchDeleteDepartmentViewTest setup test

--- departments/tests/tests_views.py
+++ departments/tests/tests_views.py
@@ -72,3 +72,40 @@ class CreateDepartmentViewTest(APITestCase):
         self.assertEqual(400, response.status_code)
         self.assertIn("name", response.data)
         self.assertEqual("required", response.data["name"][0].code)
+
+
+class PatchDeleteDepartmentViewTest(APITestCase):
+    @classmethod
+    def setUpTestData(cls) -> None:
+        cls.user_admin = User.objects.create_superuser(
+            username="AdminUser", password="1234"
+        )
+        cls.user_non_admin = User.objects.create_user(
+            username="NonAdminUser", password="1234"
+        )
+
+        cls.user_admin_data_login = {
+            "username": cls.user_admin.username,
+            "password": "1234",
+        }
+
+        cls.user_non_admin_data_login = {
+            "username": cls.user_non_admin.username,
+            "password": "1234",
+        }
+
+        cls.department_patch_data = {
+            "name": "Nome Atualizado",
+        }
+
+    def setUp(self) -> None:
+        login_admin = self.client.post("/api/login/", self.user_admin_data_login)
+        self.token_admin = f"Token {login_admin.data['token']}"
+
+        login_non_admin = self.client.post(
+            "/api/login/", self.user_non_admin_data_login
+        )
+        self.token_non_admin = f"Token {login_non_admin.data['token']}"
+
+        department_response = self.client.post("/api/department/", {"name": "TI"})
+        self.department = department_response.data
