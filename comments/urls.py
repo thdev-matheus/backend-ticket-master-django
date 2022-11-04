6790f81093ea97ab19e4a61617261c9aa30963b5
Andre Perregil <andre_perregil@outlook.com>
2022-11-04 15:08:14 +0000

    Fix: solving conflicts

--- comments/urls.py
+++ comments/urls.py
@@ -3,11 +3,7 @@ from django.urls import path
 from comments import views
 
 urlpatterns = [
-<<<<<<< HEAD
-    #path("comment/", views.ListAllCommentsView.as_view()),
-=======
     # path("comment/", views.ListAllCommentsView.as_view()),
->>>>>>> 985e39a4d2a9fb9dd9edb6b6d9bd4221e8c73163
     path("comment/register/<ticket_id>/", views.CreateCommentView.as_view()),
     path("comment/<comment_id>/", views.GetIDPatchDeleteCommentView.as_view()),
     path("comment/user/<user_id>/", views.GetUserCommentsView.as_view()),
