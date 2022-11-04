21efcb30d43c0d5455a2c43be116f091690b734b
Theus <matheusth.dev@gmail.com>
2022-11-04 10:34:24 -0300

    heroku config
    Signed-off-by: Theus <matheusth.dev@gmail.com>

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+from .models import User
+
+
+class CustomUser(UserAdmin):
+    readonly_fields = ("updated_at", "date_joined", "last_login")
+
+    fieldsets = (
+        ("Credentials", {"fields": ("username", "password")}),
+        (
+            "Personal Info",
+            {"fields": ("email", "department")},
+        ),
+        ("Permissions", {"fields": ("is_superuser", "is_staff", "is_active")}),
+        ("Dates", {"fields": ("updated_at", "date_joined", "last_login")}),
+    )
+
+
+admin.site.register(User, CustomUser)
